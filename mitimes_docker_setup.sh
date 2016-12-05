73d5ff2de5ad995344dfb6be6360b1e3eac7a155
Adam Davies <adzdavies@gmail.com>
2016-12-05 15:09:30 +1030

    Comments

--- mitimes_docker_setup.sh
+++ mitimes_docker_setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+echo Setting up '.env' requires AWS -- this is used when recompiling assets.
+echo .
 echo Input AWS Secret Key ID:
 read aws_secret_key_id
 echo Input AWS Secret Key:
 read aws_secret_key
 
 
+# Clone apps
 mkdir -p ~/sites
 cd ~/sites
 git clone git@github.com:mitimes/yggdrasil.git
@@ -16,22 +19,27 @@ git clone git@github.com:mitimes/client-hub.git
 git clone git@github.com:mitimes/docker-postgresql.git
 
 
+# Setup aliases in bash
 echo "alias dc='docker-compose'" >> ~/.bash_profile
 echo "alias dcupd='dc up -d'" >> ~/.bash_profile
 echo "alias dstopa='docker stop $(docker ps -q)'" >> ~/.bash_profile
 echo "alias dcstop='dc stop'" >> ~/.bash_profile
 echo "alias dcrestart='dcstop && dcupd'" >> ~/.bash_profile
 echo "alias mitimesstop='cd ~/sites && cd yggdrasil && dcstop && cd .. && cd api && dcstop && cd .. && cd web && dcstop && cd .. && cd client-hub && dcstop && cd ..'" >> ~/.bash_profile
-echo "alias mitimescleanup='if [ -f ~/sites/web/tmp/pids/server.pid ] ; then 
-rm ~/sites/web/tmp/pids/server.pid 
-fi && if [ -f ~/sites/client-hub/tmp/pids/server.pid ] ; then 
-rm ~/sites/client-hub/tmp/pids/server.pid 
+echo "alias mitimescleanup='if [ -f ~/sites/web/tmp/pids/server.pid ] ; then
+rm ~/sites/web/tmp/pids/server.pid
+fi && if [ -f ~/sites/client-hub/tmp/pids/server.pid ] ; then
+rm ~/sites/client-hub/tmp/pids/server.pid
 fi'" >> ~/.bash_profile
 echo "alias mitimesup='cd ~/sites && cd yggdrasil && dcupd && cd .. && cd api && dcupd && cd .. && cd web && dcupd && cd .. && cd client-hub && dcupd && cd ..'" >> ~/.bash_profile
 echo "alias mitimesrestart='mitimesstop && mitimescleanup && mitimesup'" >> ~/.bash_profile
 
+
+# Load up db
+echo "Building postgresql docker..."
 cd ~/sites/docker-postgresql
-docker build -t mitimes/db . 
+docker build -t mitimes/db .
+
 
 # FOR RECOMPILE ASSETS
 cat > ~/sites/web/.env <<-EOM
